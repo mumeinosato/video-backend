950243bc5df26afed073635466febf30cbfa57c8
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-03-16 22:11:01 +0900

    ビルドするように

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+cd video-backend
+docker-compose down
+cd ..
+
+rm -rf video-backend
+git clone https://github.com/mumeinosato/video-backend.git
+cp .env video-backend/.env
+
+cd video-backend
 docker-compose up --build
