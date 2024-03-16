6ec58256670c16f365421a4b5ebfb4b7ebdca0f3
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-03-17 07:10:27 +0900

    env追加

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,9 @@ FROM node:18.12 as runner
 WORKDIR /work
 EXPOSE 3000
 
+ENV NODE_ENV dev
+ENV PUBLIC_PATH_NAS /mnt/nas/share/
+
 COPY package*.json ./
 RUN npm install --production && npm cache clean --force
 
--- run.sh
+++ run.sh
@@ -7,4 +7,10 @@ git clone https://github.com/mumeinosato/video-backend.git
 cp .env video-backend/.env
 
 cd video-backend
+rm Dockerfile
+cd ..
+
+cp Dockerfile video-backend/Dockerfile
+
+cd video-backend
 docker-compose up --build
